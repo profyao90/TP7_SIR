30b34ef29c3ff5d9914a4e1f03488fe3983666be
profyao90 <aubinya90@gmail.com>
2018-04-07 16:41:08 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sir7App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sir7App
+ */
+angular.module('sir7App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sir7App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sir7App
+ */
+angular.module('sir7App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
